8f2a20ec153c2f7e86e113c3264bb936f38b218c
holynova-SD <holyalakir@outlook.com>
2025-05-16 15:34:23 +0800

    [modify] 1、修复接口中的一个与框架逻辑不适配的地方；

--- test/test_interface_call.cpp
+++ test/test_interface_call.cpp
@@ -9,6 +9,7 @@
 #include <numeric>
 #include <iomanip>
 #include <omp.h>
+#include <cmath>
 
 #include "sdk/bi_sdk_api.h"
 
@@ -16,6 +17,7 @@ namespace test_interface_call
 {
     template <typename T>
     void find_and_print_top_n(std::vector< std::vector<T> > &vec, const int n) {
+        // std::cout << std::fixed << std::setprecision(2);
         for (size_t i = 0; i < vec.size(); ++i) {
             std::vector<int> tmp_vec(vec[i].size());
             std::iota(tmp_vec.begin(), tmp_vec.end(), 0);
@@ -25,7 +27,38 @@ namespace test_interface_call
 
             std::cout << "item " << i << ": " << std::endl;
             for (int k = 0; k < n; ++k) {
-                std::cout << "(" << tmp_vec[k] << ", " << std::fixed << std::setprecision(2) << vec[i][tmp_vec[k]] << ")";
+                std::cout << "(" << tmp_vec[k] << ", " << vec[i][tmp_vec[k]] << ")";
+                if (k != n - 1) {
+                    std::cout << " | ";
+                }
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    void find_and_print_top_n_softmax_log(std::vector< std::vector<float> >& vec, const int n) {
+        for (size_t i = 0; i < vec.size(); ++i) {
+            std::vector<float> softmax_log_vec;
+            softmax_log_vec.reserve(vec[i].size());
+            double sum_exp = 0;
+            for (float f_num : vec[i]) {
+                sum_exp += exp(f_num);
+            }
+            double softmax_scale = 1.0f / sum_exp;
+            for (float f_num : vec[i]) {
+                auto after_softmax = static_cast<float>(exp(f_num) * softmax_scale);
+                softmax_log_vec.emplace_back(-1 * logf(after_softmax));
+            }
+
+            std::vector<int> tmp_vec(vec[i].size());
+            std::iota(tmp_vec.begin(), tmp_vec.end(), 0);
+            std::sort(tmp_vec.begin(), tmp_vec.end(), [&](const int a, const int b) {
+                return softmax_log_vec[a] < softmax_log_vec[b];
+            });
+
+            std::cout << "item " << i << ": " << std::endl;
+            for (int k = 0; k < n; ++k) {
+                std::cout << "(" << tmp_vec[k] << ", " << softmax_log_vec[tmp_vec[k]] << ")";
                 if (k != n - 1) {
                     std::cout << " | ";
                 }
@@ -68,15 +101,17 @@ TEST(InterfaceCall, Gpt2Model)
 
     model_interface->bi_run(output_vec);
 
-    test_interface_call::find_and_print_top_n(output_vec, 20);
+    // test_interface_call::find_and_print_top_n(output_vec, 20);
+    // test_interface_call::find_and_print_top_n_softmax_log(output_vec, 20);
 
-    std::vector< std::vector<unsigned int> > input_vec2 = { { 0, 1, 2, }, { 0, 1, 2 }, { 0, 1, 2 } };
+    std::vector< std::vector<unsigned int> > input_vec2 = { { 0 }, { 0, 1 }, { 0, 1, 2 } };
 
     model_interface->bi_set_input(input_vec2);
 
     model_interface->bi_run(output_vec);
 
     test_interface_call::find_and_print_top_n(output_vec, 20);
+    test_interface_call::find_and_print_top_n_softmax_log(output_vec, 20);
 
     DeleteBIModelInterface(model_interface);
 
